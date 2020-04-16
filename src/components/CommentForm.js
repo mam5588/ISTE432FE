e3c3ac7b346c4f981fd0c1ea1e7bd5d9512b4879
KajalNag <kxn5733@g.rit.edu>
2020-04-16 19:32:02 -0400

    CommentForm started

---
+++ src/components/CommentForm.js
@@ -0,0 +1,32 @@
+import Component from 'react';
+
+class CommentForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A comment was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Comment:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
