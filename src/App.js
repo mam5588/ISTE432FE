1a983f2a09ec15800424c45db152f35e777fe683
pikachungg <34949943+pikachungg@users.noreply.github.com>
2020-04-16 15:57:21 -0500

    components

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+		<div className="navigation">
+			
+		</div>
     </div>
   );
 }
---
+++ src/components/Playlist.js
@@ -0,0 +1,13 @@
+import Component from 'react';
+
+class Playlist extends Component {
+    render(){
+        return(
+            <div className="playlist">
+
+            </div>
+        );
+    }
+}
+
+export default Playlist
